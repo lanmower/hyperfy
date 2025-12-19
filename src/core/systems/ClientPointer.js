45fd4ef6efec6550ebccb8b78d942a1d5c8bce26
User <user@example.com>
2025-12-19 08:13:22 +0200

    Phase 3.36: ClientPointer.js modularization (164L → 41L, 123L reduction)
    - Extract PointerEventManager: Complete pointer event handling, path tracking, cursor management
    - Consolidates PointerState, PointerEvent classes and all pointer event logic
    - ClientPointer now delegates all event management to PointerEventManager
    - 75% LOC reduction (164L → 41L)
    - Syntax validated

--- src/core/systems/ClientPointer.js
+++ src/core/systems/ClientPointer.js
@@ -1,8 +1,7 @@
 import * as THREE from '../extras/three.js'
 import { ControlPriorities } from '../extras/ControlPriorities.js'
-
 import { System } from './System.js'
-
+import { PointerEventManager } from './pointer/PointerEventManager.js'
 
 export class ClientPointer extends System {
   static DEPS = {
@@ -12,7 +11,7 @@ export class ClientPointer extends System {
 
   constructor(world) {
     super(world)
-    this.pointerState = new PointerState()
+    this.pointerState = new PointerEventManager()
   }
 
   init({ ui }) {
@@ -38,127 +37,5 @@ export class ClientPointer extends System {
   destroy() {
     this.control?.release()
     this.control = null
-  }
-}
-
-const PointerEvents = {
-  ENTER: 'pointerenter',
-  LEAVE: 'pointerleave',
-  DOWN: 'pointerdown',
-  UP: 'pointerup',
-}
-
-const CURSOR_DEFAULT = 'default'
-
-class PointerEvent {
-  constructor() {
-    this.type = null
-    this._propagationStopped = false
-  }
-
-  set(type) {
-    this.type = type
-    this._propagationStopped = false
-  }
-
-  stopPropagation() {
-    this._propagationStopped = true
-  }
-}
-
-class PointerState {
-  constructor() {
-    this.activePath = new Set()
-    this.event = new PointerEvent()
-    this.cursor = CURSOR_DEFAULT
-    this.pressedNodes = new Set()
-  }
-
-  update(hit, pointerPressed, pointerReleased) {
-    const newPath = hit ? this.getAncestorPath(hit) : []
-    const oldPath = Array.from(this.activePath)
-
-    let i = 0
-    while (i < newPath.length && i < oldPath.length && newPath[i] === oldPath[i]) i++
-
-    for (let j = oldPath.length - 1; j >= i; j--) {
-      if (oldPath[j].onPointerLeave) {
-        this.event.set(PointerEvents.LEAVE)
-        try {
-          oldPath[j].onPointerLeave?.(this.event)
-        } catch (err) {
-          console.error(err)
-        }
-      }
-      this.activePath.delete(oldPath[j])
-    }
-
-    for (let j = i; j < newPath.length; j++) {
-      if (newPath[j].onPointerEnter) {
-        this.event.set(PointerEvents.ENTER)
-        try {
-          newPath[j].onPointerEnter?.(this.event)
-        } catch (err) {
-          console.error(err)
-        }
-        if (this.event._propagationStopped) break
-      }
-      this.activePath.add(newPath[j])
-    }
-
-    let cursor = CURSOR_DEFAULT
-    if (newPath.length > 0) {
-      for (let i = newPath.length - 1; i >= 0; i--) {
-        if (newPath[i].cursor) {
-          cursor = newPath[i].cursor
-          break
-        }
-      }
-    }
-    if (cursor !== this.cursor) {
-      document.body.style.cursor = cursor
-      this.cursor = cursor
-    }
-
-    if (pointerPressed) {
-      for (let i = newPath.length - 1; i >= 0; i--) {
-        const node = newPath[i]
-        if (node.onPointerDown) {
-          this.event.set(PointerEvents.DOWN)
-          try {
-            node.onPointerDown(this.event)
-          } catch (err) {
-            console.error(err)
-          }
-          this.pressedNodes.add(node)
-          if (this.event._propagationStopped) break
-        }
-      }
-    }
-
-    if (pointerReleased) {
-      for (const node of this.pressedNodes) {
-        if (node.onPointerUp) {
-          this.event.set(PointerEvents.UP)
-          try {
-            node.onPointerUp(this.event)
-          } catch (err) {
-            console.error(err)
-          }
-          if (this.event._propagationStopped) break
-        }
-      }
-      this.pressedNodes.clear()
-    }
-  }
-
-  getAncestorPath(hit) {
-    const path = []
-    let node = hit.node?.resolveHit?.(hit) || hit.node
-    while (node) {
-      path.unshift(node)
-      node = node.parent
-    }
-    return path
   }
 }
