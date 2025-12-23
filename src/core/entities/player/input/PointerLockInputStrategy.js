d095974c0beeb3def4bbe8c70d5f7f05630338cb
User <user@example.com>
2025-12-23 16:31:36 +0200

    Fix: Update camera quaternion from Euler rotation angles
    - Added quaternion.setFromEuler(rotation) after updating Euler angles in pointer lock strategy
    - Camera rotation changes from mouse input now properly sync to quaternion
    - Fixes: World camera now correctly follows player camera rotation
    - Player can now look around using mouse with pointer lock enabled

--- src/core/entities/player/input/PointerLockInputStrategy.js
+++ src/core/entities/player/input/PointerLockInputStrategy.js
@@ -5,11 +5,12 @@ import { DEG2RAD } from '../../../extras/general.js'
 
 export class PointerLockInputStrategy extends InputStrategy {
   updateLook(delta, control, context) {
-    const { rotation } = this.camera
+    const { rotation, quaternion } = this.camera
     rotation.x += -control.pointer.delta.y * POINTER_LOOK_SPEED * delta
     rotation.y += -control.pointer.delta.x * POINTER_LOOK_SPEED * delta
     rotation.z = 0
     rotation.x = clamp(rotation.x, MIN_PITCH_ANGLE * DEG2RAD, MAX_PITCH_ANGLE * DEG2RAD)
+    quaternion.setFromEuler(rotation)
   }
 
   updateZoom(delta, control) {
