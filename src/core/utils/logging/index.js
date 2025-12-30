d2b1e3b01f9aaa10c91f82601fa53de9a1538d3f
User <user@example.com>
2025-12-30 21:16:04 +0200

    Phase 2: Consolidate logging system (-230 LOC)

---
+++ src/core/utils/logging/index.js
@@ -0,0 +1,3 @@
+export { ComponentLogger } from './ComponentLogger.js'
+export { StructuredLogger } from './StructuredLogger.js'
+export { LogLevels, LogLevelNames, getLevelValue, shouldLog } from './LogLevels.js'
---
+++ src/server/logging/index.js
@@ -0,0 +1,2 @@
+export { Logger, ConsoleSink, FileSink } from './Logger.js'
+export * from './IntegrationUtils.js'
