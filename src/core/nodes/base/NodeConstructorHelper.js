fc898bf1dfd423ea0fe78b2b078f39476d6f36c8
User <user@example.com>
2025-12-30 16:04:36 +0200

    Fix: NodeConstructorHelper import syntax

--- src/core/nodes/base/NodeConstructorHelper.js
+++ src/core/nodes/base/NodeConstructorHelper.js
@@ -1,7 +1,7 @@
 /* Node constructor boilerplate abstraction for 24 node types */
+import { defineProps } from '../../utils/helpers/defineProperty.js'
 
 export function initializeNode(instance, nodeName, propertySchema, defaultData, data) {
   instance.name = nodeName
-  const { defineProps } = await import('../../utils/helpers/defineProperty.js')
   defineProps(instance, propertySchema, defaultData, data)
 }
