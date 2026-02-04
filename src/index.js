c73d40ecd3347887bfddef904d6a9e350f68c533
System <system@local>
2026-02-04 14:59:11 +0200

    Final: Ruthless audit - minimal physics SDK (2 files, 98 LOC, 1 dependency)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-// Pure physics SDK - zero HTTP, zero infrastructure
-// Export clean SDK functions for physics simulation
-
-export { createWorld } from './physics/World.js'
-export { loadGLB } from './physics/GLBLoader.js'
-export { addBody } from './physics/BodyManager.js'
-export { raycast, castRay } from './physics/Queries.js'
-export { step, simulate } from './physics/Simulation.js'
-export { getBodyData, getEntityData } from './physics/DataAccess.js'
-export { Vector3, Quaternion, Transform } from './math/index.js'
+export { createWorld, addBody, step, raycast, getEntityData } from './physics/World.js'
