5f16b4ac798b7d09c47e57b86a02955cb70cf2f3
System <system@local>
2026-02-05 11:01:02 +0200

    simplify: Remove unused code, ~379 lines total

--- src/client/ReconciliationEngine.js
+++ src/client/ReconciliationEngine.js
@@ -2,23 +2,8 @@ export class ReconciliationEngine {
   constructor(config = {}) {
     this.correctionThreshold = config.correctionThreshold || 0.01
     this.correctionSpeed = config.correctionSpeed || 0.5
-    this.stateHistory = []
-    this.maxHistorySize = 128
     this.lastReconcileTime = 0
     this.reconcileInterval = config.reconcileInterval || 100
-  }
-
-  recordPredictedState(tick, state) {
-    this.stateHistory.push({
-      tick,
-      state: JSON.parse(JSON.stringify(state)),
-      timestamp: Date.now(),
-      confirmed: false
-    })
-
-    if (this.stateHistory.length > this.maxHistorySize) {
-      this.stateHistory.shift()
-    }
   }
 
   reconcile(serverState, localState, tick) {
@@ -50,55 +35,20 @@ export class ReconciliationEngine {
   }
 
   generateCorrection(serverState, localState) {
-    const correction = {
+    return {
       position: [
         serverState.position[0] * this.correctionSpeed + localState.position[0] * (1 - this.correctionSpeed),
         serverState.position[1] * this.correctionSpeed + localState.position[1] * (1 - this.correctionSpeed),
         serverState.position[2] * this.correctionSpeed + localState.position[2] * (1 - this.correctionSpeed)
       ],
-      velocity: JSON.parse(JSON.stringify(serverState.velocity)),
+      velocity: [...serverState.velocity],
       onGround: serverState.onGround
     }
-
-    return correction
   }
 
   applyCorrection(localState, correction) {
     localState.position = correction.position
     localState.velocity = correction.velocity
     localState.onGround = correction.onGround
-  }
-
-  resimulate(predictionEngine, fromTick) {
-    const inputHistory = predictionEngine.getInputHistory()
-
-    for (const input of inputHistory) {
-      predictionEngine.predict(input.data)
-    }
-  }
-
-  detectRollback(serverTick, lastConfirmedTick) {
-    return serverTick <= lastConfirmedTick
-  }
-
-  getStateAtTick(tick) {
-    for (let i = this.stateHistory.length - 1; i >= 0; i--) {
-      if (this.stateHistory[i].tick <= tick) {
-        return this.stateHistory[i]
-      }
-    }
-    return null
-  }
-
-  confirmState(tick) {
-    for (const state of this.stateHistory) {
-      if (state.tick === tick) {
-        state.confirmed = true
-      }
-    }
-  }
-
-  clearHistory() {
-    this.stateHistory = []
   }
 }
