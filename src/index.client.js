decb5e349ab5762ff9c61d4bb5574b341d374f91
System <system@local>
2026-02-05 16:26:21 +0200

    fix: Resolve jolt-physics module resolution error with separate entry points

---
+++ src/index.client.js
@@ -0,0 +1,9 @@
+export { InputHandler } from './client/InputHandler.js'
+export { PhysicsNetworkClient } from './client/PhysicsNetworkClient.js'
+export { PredictionEngine } from './client/PredictionEngine.js'
+export { ReconciliationEngine } from './client/ReconciliationEngine.js'
+export { RenderSync } from './client/RenderSync.js'
+
+export { MSG, msgName, DISCONNECT_REASONS, CONNECTION_QUALITY } from './protocol/MessageTypes.js'
+export { Codec } from './protocol/Codec.js'
+export { SequenceTracker } from './protocol/SequenceTracker.js'
